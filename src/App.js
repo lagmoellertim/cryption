98dd196b5150dabc2716a2357b7a0f58b2738a10
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2019-02-19 19:45:27 +0100

    Added CSP Header

--- src/App.js
+++ src/App.js
@@ -9,15 +9,37 @@ import { createStore } from 'redux';
 import reducer from './store/reducer';
 import devToolsEnhancer from 'remote-redux-devtools';
 import Footer from './components/footer/Footer';
+import MetaTags from 'react-meta-tags';
 
 class App extends Component {
 	constructor() {
 		super();
-		this.store = createStore(reducer, devToolsEnhancer());
+		this.developmentMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+		if (this.developmentMode) {
+			this.store = createStore(reducer, devToolsEnhancer());
+		} else {
+			this.store = createStore(reducer);
+		}
 	}
 	render() {
 		return (
 			<Provider store={this.store}>
+				{this.developmentMode ? null : (
+					<MetaTags>
+						<meta
+							httpEquiv="Content-Security-Policy"
+							content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; style-src https://fonts.googleapis.com; img-src 'self'; media-src 'none'; frame-src 'none'; font-src https://fonts.gstatic.com; connect-src 'none'"
+						/>
+						<meta
+							httpEquiv="X-Content-Security-Policy"
+							content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; style-src https://fonts.googleapis.com; img-src 'self'; media-src 'none'; frame-src 'none'; font-src https://fonts.gstatic.com; connect-src 'none'"
+						/>
+						<meta
+							httpEquiv="X-WebKit-CSP"
+							content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; style-src https://fonts.googleapis.com; img-src 'self'; media-src 'none'; frame-src 'none'; font-src https://fonts.gstatic.com; connect-src 'none'"
+						/>
+					</MetaTags>
+				)}
 				<Layout className={AppCSS.layout}>
 					<Header />
 					<Body />
