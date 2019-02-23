fc667c46b36c155e5bd4a95bd1117dbefef4629b
lagmoellertim <lagmoellertim@gmail.com>
2019-02-23 20:54:18 +0000

    commit d727af00cdfd4c73eb9fa0c3abd6a7521dc42010 Author: Tim-Luca Lagmöller <lagmoellertim@gmail.com> Date:   Sat Feb 23 21:52:50 2019 +0100
        Create CODE_OF_CONDUCT.md

---
+++ precache-manifest.56cc9b5d97ae438f2187611244f99f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58247b25f2b7ab410d3e",
+    "url": "./static/css/main.6010f166.chunk.css"
+  },
+  {
+    "revision": "58247b25f2b7ab410d3e",
+    "url": "./static/js/main.58247b25.chunk.js"
+  },
+  {
+    "revision": "64770691dee15f20c0ce",
+    "url": "./static/js/runtime~main.64770691.js"
+  },
+  {
+    "revision": "85e9e547d64eaea150e0",
+    "url": "./static/css/2.492f9e34.chunk.css"
+  },
+  {
+    "revision": "85e9e547d64eaea150e0",
+    "url": "./static/js/2.85e9e547.chunk.js"
+  },
+  {
+    "revision": "3c672e9af9dc7ac9779a1c34b942a364",
+    "url": "./de9f35e9708bdb867efb.worker.js"
+  },
+  {
+    "revision": "51ec01cd96cae524f12d58e14274cac8",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.794047fb84a64b247d75a9e7316b5934.js"
+  "./precache-manifest.56cc9b5d97ae438f2187611244f99f80.js"
 );
 
 workbox.clientsClaim();
