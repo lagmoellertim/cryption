9bae66e818180343bf5a0aabe9ec4368b65b25f5
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2019-02-28 20:29:11 +0100

    Use zxcvbn for Password Strength Check

--- src/components/modal/PasswordModal/PasswordMeter/PasswordMeter.jsx
+++ src/components/modal/PasswordModal/PasswordMeter/PasswordMeter.jsx
@@ -4,26 +4,10 @@ import * as PasswordMeterLogic from "./PasswordMeter.logic";
 
 export default ({ password }) => {
 	const strength = PasswordMeterLogic.scorePassword(password);
-	const strengthMap = {
-		null: PasswordMeterCSS.invisible,
-		0: PasswordMeterCSS.weak,
-		1: PasswordMeterCSS.good,
-		2: PasswordMeterCSS.strong
-	};
-
-	const getMatchingClass = () => {
-		const index = strength;
-		if (strength in strengthMap) {
-			return strengthMap[index];
-		}
-		return PasswordMeterCSS.invisible;
-	};
-
-	const strengthClass = getMatchingClass();
 
 	return (
 		<div className={PasswordMeterCSS.meter}>
-			<div className={PasswordMeterCSS.bar + " " + strengthClass} />
+			<div className={PasswordMeterCSS.bar + " " + PasswordMeterCSS[strength]} />
 		</div>
 	);
 };
