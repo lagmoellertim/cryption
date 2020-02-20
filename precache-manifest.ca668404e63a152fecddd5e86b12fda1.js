ca03f913f979de0d0baa08f9bea4a8cc276d54e7
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2020-02-20 17:23:58 +0000

    commit 13d81de51601db27b53d1c5ce77e6d2a33fdea90 Author: Tim-Luca Lagmöller <lagmoellertim@gmail.com> Date:   Thu Feb 20 18:21:58 2020 +0100
        Update README.md

---
+++ precache-manifest.ca668404e63a152fecddd5e86b12fda1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "37c66b038a446e2dd9a2",
+    "url": "./static/css/main.b50f7a92.chunk.css"
+  },
+  {
+    "revision": "37c66b038a446e2dd9a2",
+    "url": "./static/js/main.37c66b03.chunk.js"
+  },
+  {
+    "revision": "9ffb70c93509eef4f423",
+    "url": "./static/js/runtime~main.9ffb70c9.js"
+  },
+  {
+    "revision": "f06f7a0190b9f33d1bf7",
+    "url": "./static/css/2.a7ad2eab.chunk.css"
+  },
+  {
+    "revision": "f06f7a0190b9f33d1bf7",
+    "url": "./static/js/2.f06f7a01.chunk.js"
+  },
+  {
+    "revision": "c4a7fd0749c6cc9c330e017e378bbbff",
+    "url": "./2a22b21bc89fdb6be083.worker.js"
+  },
+  {
+    "revision": "01c3dd0c15f771cae0e628abbf06354b",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.59269a55ba8c8e52a96e9cf85f370b38.js"
+  "./precache-manifest.ca668404e63a152fecddd5e86b12fda1.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
